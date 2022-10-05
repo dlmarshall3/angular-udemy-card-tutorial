10dbf35912f3dc625528d9c38b7f7e3bb90f9457
David Marshall <dlmarshall3@protonmail.com>
2022-10-05 17:02:04 -0500

    pushing to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-card-app';
+  posts = [
+    {
+      title: 'Kaiju!!!',
+      imageUrl: 'assets/garamon.png',
+      username: '@ichiro725',
+      content: 'Saw this strange monster during my hike today. I think he had rabies? #classicFilter'
+    },
+    {
+      title: 'Aliens!!!',
+      imageUrl: 'assets/aliens.jpg',
+      username: '@tru3_b3li3v3r',
+      content: 'Cannot believe aliens are eating in the public like this. I have been ridiculed for years, and now this?!'
+    },
+    {
+      title: 'Eye for detail.',
+      imageUrl: 'assets/laundry.png',
+      username: '@mons_photos',
+      content: 'I still am surprised this shot turned out the way it did.'
+    }
+  ]
 }
